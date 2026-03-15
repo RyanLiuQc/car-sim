ddd7d1852383eb71466c348fa10c18d6d947aa38
Ryan Liu <ryanliuqc@gmail.com>
2026-03-15 14:17:31 -0400

    find centerCurve with KDTree instead of a hard coded center line. generalizes to any circular race track centered at origin. does not work on ellipse like race track because the 2 nearest points between inner and outer curve is not necesarily points that are at the same percent arc length of the track

--- race_track.py
+++ race_track.py
@@ -1,21 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from scipy.spatial import KDTree
 
 import bicycle_model
 
 
 class RaceTrack:
 
-    # the curves are polylines
+    # the curves are polylines 
     def __init__(
             self, 
-            outerCurve: list[tuple[int,int]],
-            centerCurve: list[tuple[int,int]], 
-            innerCurve: list[tuple[int,int]]):
+            outerCurve: list[tuple[int,int]], # list of points (x,y)
+            # centerCurve: list[tuple[int,int]], 
+            innerCurve: list[tuple[int,int]]
+            ):
+        
         self.outerCurve = outerCurve
-        self.centerCurve = centerCurve
         self.innerCurve = innerCurve
+        
+        # get centerCurve
+        self.centerCurve = self._get_centerCurve()
+
+    def _get_centerCurve(self) -> list[tuple[int,int]]:
+        tree = KDTree(self.outerCurve)
+        centerCurve = []
+        
+        # for every point in inner, find its nearest neighbor out of all the outer points
+        # calculate the the center point
+        for pt in self.innerCurve:
+            dist, idx = tree.query(pt) # idx = index of the outer point closest to pt
+            q = self.outerCurve[idx]
+            
+            # calculate center point between inner and outer point
+            x_center = (pt[0] + q[0])/2 
+            y_center = (pt[1] + q[1])/2
+            
+            if pt[1] == 0:
+                print("x1x2: ",pt[0], q[0])
+                print("y1y2: ",pt[1], q[1])
+                print(x_center, y_center)
+
+            centerCurve.append((x_center, y_center))
+        
+        
+        return centerCurve
     
     def visualize(self):
         xs, ys = zip(*self.outerCurve)
@@ -29,9 +58,11 @@ class RaceTrack:
 
         plt.show()
 
+        
+
 # helper function to generate a polyline of an ellipse
-def get_positive_y_value_of_ellipse_at_x(x: int, a: int, b: int) -> int:
-    return round(math.sqrt((1 - (x/a)**2) * (b**2)))
+def get_positive_y_value_of_ellipse_at_x(x: int, a: int, b: int) -> float:
+    return (math.sqrt((1 - (x/a)**2) * (b**2))) # round for discrete
 
 def build_ellipse_polyline( a: int,b: int) -> list[tuple[int,int]]:
     curve = []
@@ -42,7 +73,7 @@ def build_ellipse_polyline( a: int,b: int) -> list[tuple[int,int]]:
         curve.append((x,y))
     
     # generate negative y points for the curves
-    number_of_points_in_half_curve = a*2+1 # same as len(curve) so far
+    number_of_points_in_half_curve = a*2 + 1 # same as len(curve) so far
     for i in range(1,number_of_points_in_half_curve):
         pt = curve[number_of_points_in_half_curve-1-i]
         curve.append((pt[0], -pt[1]))
@@ -52,13 +83,13 @@ def build_ellipse_polyline( a: int,b: int) -> list[tuple[int,int]]:
 
 if __name__ == "__main__":
     outerCurve = build_ellipse_polyline(20,10)
-    centerCurve = build_ellipse_polyline(15,7)
+    # centerCurve = build_ellipse_polyline(15,7)
     innerCurve = build_ellipse_polyline(10,5)
     # xs, ys = zip(*outerCurve)
 
 
     # plt.plot(xs, ys)
     # plt.show()
-    environment = RaceTrack(outerCurve, centerCurve, innerCurve)
+    environment = RaceTrack(outerCurve, innerCurve)
     environment.visualize()
 
